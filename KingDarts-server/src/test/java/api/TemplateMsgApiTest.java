cbca6bb09a29e61cb80bb1b12c8851eaa182d073
L-weixin <547121744@qq.com>
2020-07-08 17:49:54 +0800

    更换测试类

--- KingDarts-server/src/test/java/api/TemplateMsgApiTest.java
+++ KingDarts-server/src/test/java/api/TemplateMsgApiTest.java
@@ -1,8 +1,12 @@
 package api;
 
+import com.github.pagehelper.Page;
 import com.jfinal.weixin.sdk.api.ApiResult;
 import com.jfinal.weixin.sdk.api.TemplateData;
 import com.jfinal.weixin.sdk.api.TemplateMsgApi;
+import org.springframework.data.crossstore.HashMapChangeSet;
+
+import java.util.*;
 
 /**
  * 模板消息测试
@@ -13,12 +17,14 @@ import com.jfinal.weixin.sdk.api.TemplateMsgApi;
  */
 public class TemplateMsgApiTest {
 
-	
+    public static String loadSetMethodName(String filedName){
+        return "set" + filedName.substring(0, 1).toUpperCase() + filedName.substring(1);
+    }
 	//sssssss
     public static void main(String[] args) {
         // 模板消息，发送测试：pass
 //    	https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1433751277
-        ApiResult result = TemplateMsgApi.send(TemplateData.New()
+        /*ApiResult result = TemplateMsgApi.send(TemplateData.New()
             // 消息接收者
             .setTouser("oOGf-jgjmwxFVU66D-lFO2AFK8ic")
             // 模板id
@@ -35,6 +41,25 @@ public class TemplateMsgApiTest {
             .add("remark", "\nxxxxxxxxxx。", "#999")
             .build());
 
-        System.out.println(result);
+        System.out.println(result);*/
+//        HashMap<String,String> map = new HashMap<>();
+//        map.put("1","11");
+//        map.put("2","222");
+//        map.get("3");
+//    //    map.clear();
+//        map=null;
+////        ArrayList<Map> list = new ArrayList<>();
+//        list.add(null);
+//        list.add(null);
+//        list.add(map);
+     //   String A =loadSetMethodName("is_publish");
+        String A ="[123,456,789,990]";
+        String ns = A.replaceAll("[^0-9a-zA-Z\u4e00-\u9fa5.，,。？“”]+","");
+        String [] B = ns.split(",");
+        List<String> list=Arrays.asList(B);
+        for(String str : list){
+            System.out.println("str"+str);
+        }
+        System.out.println("返回"+list);
     }
 }
